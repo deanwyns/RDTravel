34d58798b6e1c3261f89d1a82f01329ea7fa8e07
Dean Wyns <dean.wyns@outlook.com>
2015-10-12 17:44:04 +0200

    Style + bug fixes

--- ejb-module/src/main/java/com/realdolmen/rdtravel/domain/Flight.java
+++ ejb-module/src/main/java/com/realdolmen/rdtravel/domain/Flight.java
@@ -54,6 +54,7 @@ public class Flight {
     @Digits(fraction = 2, integer = 8)
     private BigDecimal price;
 
+    @NotNull
     @Column(nullable = false)
     @Min(value = 1)
     private Integer maxSeats;
